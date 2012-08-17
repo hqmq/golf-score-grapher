8a867408679962abb5fb43dc7a2f0b3c73c29030
Mike Ries <michael@riesd.com>
2012-08-17 00:01:44 -0400

    got a basic graph working

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,7 +4,7 @@ class GamesController < ApplicationController
   # GET /games
   # GET /games.json
   def index
-    @games = Game.all
+    @games = Game.order("played_at DESC")
 
     respond_to do |format|
       format.html # index.html.erb
--- app/models/game.rb
+++ app/models/game.rb
@@ -7,6 +7,19 @@ class Game < ActiveRecord::Base
   validates_uniqueness_of :played_at, :message => "duplicate value"
   
   def scores_hash
-    CSV.parse( self.scores, :headers => true).map {|rec| rec.to_hash }
+    @scores_hash ||= CSV.parse( self.scores, :headers => true).map {|rec| rec.to_hash }.sort_by{|obj| [obj["TOT"], obj["Player"]]}
+    @scores_hash
+  end
+  
+  def all_scores
+    scores_hash.map{|rec| rec.select{|key,val| key =~ /^H\d+/}.map{|hole, score| score} }
+  end
+  
+  def holes
+    scores_hash.first.select{|key,val| key =~ /^H\d+/ }.map{|hole, score| hole}
+  end
+  
+  def self.colors
+    ["#2f69bf","#a2bf2f","#bf5a2f","#bfa22f","#772fbf"]
   end
 end
