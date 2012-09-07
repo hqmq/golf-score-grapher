a60f517e181771a53207ba748afaa98fad89e139
Mike Ries <michael@riesd.com>
2012-09-07 00:29:42 -0700

    got rspec working with fixtures

---
+++ spec/models/game_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe Game do
+  fixtures :games
+  it "can be created" do
+    ->{
+      Game.new.should be_an_instance_of(Game)
+    }.should_not raise_exception
+  end
+end
