d5128f2c27e1e2275bfb774f741c55c0fe971a03
Mike Ries <michael@riesd.com>
2013-02-24 20:54:34 -0700

    cleaning up whitespace

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -2,16 +2,17 @@ require 'spec_helper'
 
 describe Course do
   fixtures :courses, :games, :players, :scores
-  
+
   subject{ courses(:c1) }
   it "should get a list of recent games" do
     subject.recent_games.size.should == 2
     subject.recent_games.should == [games(:g2), games(:g1)]
   end
+
   it "should calculate the par for all the holes" do
     subject.calculated_par.should == [2,2,3,3,3,3,3,3,3,2,2,2,3,3,3,4,3,2]
   end
-  
+
   describe "#records" do
     it "should get a list of course record holders" do
       subject.records.size.should == 5
@@ -19,7 +20,7 @@ describe Course do
       subject.records.first.should == top_record
       subject.records.map{|r| r.total}.should == [46,46,46,49,49]
     end
-    
+
     it "should not fail for a course with no games" do
       courses(:c2).records.should == []
     end
