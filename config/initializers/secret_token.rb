394f791eafaa099b954fc010d3ac63e43eed5a7e
Michael Ries <michael@riesd.com>
2014-01-24 22:14:47 -0700

    initial rails 4 commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GolfscoreGrapher::Application.config.secret_key_base = 'ac170ba1ffb0a266b6226761d43d506e6f891aa831de23cd9716690551d9fece909dcfb6843b99e7857436681cd274dfcff0ae450b330d9cc0d663b892156dec'
