c5a56c167f5c458dc1505962116611e8bde1be9c
hqmq <michael@riesd.com>
2014-12-31 09:26:04 -0700

    removing uneccesary view logic

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,54 +1,12 @@
 require 'csv'
 
 class GamesController < ApplicationController
-  include GamesHelper
-
-  # GET /games
-  # GET /games.json
-  def index
-    @games = game_ledger.recent(20)
-    @players = directory.most_awesome(5)
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @games }
-    end
-  end
-
   def recent
     @games = game_ledger.recent(20)
   end
 
   def show
     @game = Game.find(params[:id])
-  end
-
-  # POST /games
-  # POST /games.json
-  def create
-    @game = Game.build_from_csv(params[:file])
-
-    respond_to do |format|
-      if @game.save
-        format.html { redirect_to @game, notice: 'Game was successfully created.' }
-        format.json { render json: @game, status: :created, location: @game }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @game.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /games/1
-  # DELETE /games/1.json
-  def destroy
-    @game = Game.find(params[:id])
-    @game.destroy
-
-    respond_to do |format|
-      format.html { redirect_to games_url }
-      format.json { head :no_content }
-    end
   end
 
   private
