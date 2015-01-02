c3320af38fc75d8fb4f123724819ca25b70e59d2
hqmq <michael@riesd.com>
2015-01-02 09:35:32 -0700

    switch to expecting index.html to exist in the public directory

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    render :file => Rails.root + "db/index.html", :layout => nil
+    render :file => Rails.root + "public/index.html", :layout => nil
   end
 end
